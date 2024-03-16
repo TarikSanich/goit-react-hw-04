8e5e37ead522bba8afb7767d1aa6a931315f664f
Granovskiy Uriy <silaveri777@gmail.net>
2024-03-16 20:13:47 +0200

    10

--- src/Componentes/App/App.jsx
+++ src/Componentes/App/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from './App.module.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import SearchBar from '../SearchBar/SearchBar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import { fetchImages } from '../../image-api';
+import Loader from '../Loader/Loader';
+//npm install react-hot-toast
+import { Toaster } from 'react-hot-toast';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import ImageModal from '../ImageModal/ImageModal';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    // пропустити монтування за умовою
+    if (searchQuery === '') {
+      return;
+    }
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setError(false);
+        const data = await fetchImages(searchQuery, page);
+        setImages(prevImages => {
+          return [...prevImages, ...data];
+        });
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [searchQuery, page]);
+
+  //ф-ція submit форми
+  const handleSearch = newQuery => {
+    //перевірка чи нове query при сабміті
+    if (newQuery === searchQuery) {
+      return;
+    }
+    //початкові стани при новому сабміті
+    setSearchQuery(newQuery);
+    setPage(1);
+    setImages([]);
+    setIsLoading(true);
+    setError(false);
+    setSelectedImage(null);
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  const openModal = image => {
+    setSelectedImage(image);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={css.container}>
+      <SearchBar onSearch={handleSearch} />
+      <Toaster />
+      {error ? (
+        <ErrorMessage />
+      ) : (
+        <>
+          {images.length > 0 && (
+            <ImageGallery images={images} onImageClick={openModal} />
+          )}
+          {isLoading && <Loader />}
+          {images.length > 0 && <LoadMoreBtn onClick={handleLoadMore} />}
+          {selectedImage !== null && (
+            <ImageModal
+              isOpen={true}
+              onRequestClose={closeModal}
+              imageUrl={selectedImage.urls.regular}
+              imageAlt={selectedImage.alt}
+              likes={selectedImage.likes}
+              author={selectedImage.user.name}
+              description={selectedImage.description}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
 }
-
-export default App
